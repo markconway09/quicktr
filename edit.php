7a2276d2f5f81417a5820b9ee76460924ee7bb4f
Mark Conway <markconwaynin@gmail.com>
2024-09-06 13:03:33 +0200

    +edit iconos +version change

--- edit.php
+++ edit.php
@@ -18,16 +18,10 @@
         <!-- BOOTSTRAP -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <!-- SIGNATURE CSS -->
-        <link href="css/jquery.signature.css" rel="stylesheet">
-        <style>
-        .kbw-signature { width: 300px; height: 200px; }
-        </style>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <!-- JQUERY -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-        <!-- SIGNATURE JS -->
-        <script src="js/jquery.signature.js"></script>
         <style>
             body{
                 font-family: "comfortaa";
@@ -49,8 +43,8 @@
             </a>
             <?php
             if(isset($_SESSION["login"])){
-                echo '<a href="list.php" class="btn btn-secondary mx-2">Lista</a>';
-                echo '<a href="index.php?logout=true" class="btn btn-danger mx-2">Log Out</a>';
+                echo '<a href="list.php" class="btn btn-secondary mx-2"><i class="bi bi-columns-gap"></i> Lista</a>';
+                echo '<a href="index.php?logout=true" class="btn btn-danger mx-2"><i class="bi bi-box-arrow-in-left"></i> Log Out</a>';
             }
             ?>
         </nav>
